a6645ac66eb5872c647593a32b630591a7b5c151
chw_owo <gpdnjs2116@naver.com>
2022-02-25 11:01:24 +0900

    settings

---
+++ src/main/java/com/example/shortform/ShortformApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shortform/ShortformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shortform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
