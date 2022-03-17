cf1aee197eb40b47623170b81b9ce2d3e0c7467f
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-03-17 23:22:09 +0900

    환경설정 셋팅

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,6 +34,7 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-dev.properties,/home/ec2-user/app/application-aws.properties,/home/ec2-user/app/application-credentials.properties \
-  -Dspring.profiles.active=dev \
+  -Dspring.config.location=/home/ec2-user/app/application.yml, /home/ec2-user/app/application-prod.yml, /home/ec2-user/app/application-aws.yml, /home/ec2-user/app/application-credentials.yml \
+
+  -Dspring.profiles.active=prod \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
